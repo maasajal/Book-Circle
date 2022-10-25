ff0056b5c7a44e9d0f8a3d467173253d2ee1d21b
Md Saiful Islam <mdsaifulislam@Mds-MacBook-Air.home>
2022-10-25 13:26:43 +0300

    First look

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
+import { AppProvider } from './context.';
+import Home from './pages/Home/Home';
+import About from "./pages/About/About";
+import BookList from "./components/BookList/BookList";
+import BookDetails from "./components/BookDetails/BookDetails";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1> Book Circle </h1>
-    </div>
-  );
+    <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path =  "/" element = {<Home />}>
+          <Route path = "about" element = {<About />} />
+          <Route path = "book" element = {<BookList />} />
+          <Route path = "/book/:id" element = {<BookDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
+  )
 }
 
-export default App;
+export default App
+
